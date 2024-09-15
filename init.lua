a5b2d9f50999d38a5f9f9770754636c8aefa1570
zelong <zelong.guo@outlook.com>
2024-09-15 14:48:01 +0200

    add functions

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
--- require("config.defaults")
+--[[
+___  ___      _   _       _           _             
+|  \/  |     | \ | |     (_)         | |            
+| .  . |_   _|  \| |_   ___ _ __ ___ | |_   _  __ _ 
+| |\/| | | | | . ` \ \ / / | '_ ` _ \| | | | |/ _` |
+| |  | | |_| | |\  |\ V /| | | | | | | | |_| | (_| |
+\_|  |_/\__, \_| \_/ \_/ |_|_| |_| |_|_|\__,_|\__,_|
+         __/ |                                      
+        |___/                                       
+                                                    
+ Author: Zelong Guo @ GFZ, Potsdam
+ Email: zelong.guo@outlook.com 
+--]]
+
+
+
+require("config.defaults")
 require("config.keymaps")
+require("config.functions")
 -- require("config.plugins")
