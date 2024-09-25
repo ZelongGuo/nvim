d5c22c739a5968898f1caeb463b41ec71c600f1b
zelong <zelong.guo@outlook.com>
2024-09-26 00:29:57 +0200

    change to startify theme

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -1,3 +1,49 @@
+return {
+	'goolord/alpha-nvim',
+
+	dependencies = 'nvim-tree/nvim-web-devicons',
+
+	config = function ()
+		local alpha = require("alpha")
+		local startify = require("alpha.themes.startify")
+
+		-- Set Header, here we use startify theme rather than dashboard to keep the contents to left
+		startify.section.header.val = {
+			[[*-----------*                                                ]],
+			[[| Look what?|                                                ]],
+			[[*-----------*                                                ]],
+			[[    o             boing         boing         boing          ]],
+			[[     o  e-e           . - .         . - .         . - .      ]],
+			[[       (\_/)\       '       `.   ,'       `.   ,'       .    ]],
+			[[        `-'\ `--.___,         . .           . .          .   ]],
+			[[           '\( ,_.-'                                      .  ]],
+			[[              \\               "             "            "  ]],
+			[[              ^'^                                            ]],
+		}
+
+		-- customized my own buttons
+		startify.section.top_buttons.val = {
+			startify.button("  e   ", "  New file", ":ene <BAR> startinsert <CR>"),
+			startify.button("LDR fr", "  Recent file", ":Telescope oldfiles<CR>"),
+			startify.button("LDR ff", "󰈞  Find file", ":Telescope find_files<CR>"),
+			startify.button("  q   ", "󰅚  Quit NVIM", ":qa<CR>"),
+		}
+
+		-- remove part of `startify` defaults
+		startify.section.bottom_buttons.val = {}  -- do not need bottom buttons
+    startify.section.mru.val = {}             -- disable MRU
+		-- startify.section.mru_cwd.val = {}      -- disable MRU cwd
+
+		-- Add footer showing the number of loaded plugins
+		local lazy = require("lazy")
+		startify.section.footer.val = { { type = "text", val = "🚀 Lazy Message 󰒲 : " .. lazy.stats().count .. " plugins already loaded ..."} }
+
+		-- apply current configurations
+		alpha.setup(startify.config)
+	end,
+}
+
+
 -- return {
 -- 	'goolord/alpha-nvim',
 -- 	
@@ -44,42 +90,46 @@
 --   end,
 -- }
 
-return {
-	'goolord/alpha-nvim',
-	
-	-- dependencies = { 'echasnovski/mini.icons' },
-	dependencies = 'nvim-tree/nvim-web-devicons',
 
-	config = function ()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+--return {
+--	'goolord/alpha-nvim',
+--	
+--	-- dependencies = { 'echasnovski/mini.icons' },
+--	dependencies = 'nvim-tree/nvim-web-devicons',
+--
+--	config = function ()
+--		local alpha = require("alpha")
+--		local dashboard = require("alpha.themes.dashboard")
+--
+--		-- Set Header
+--		dashboard.section.header.val = {
+--			[[*-----------*                                                ]],
+--			[[| Look what?|                                                ]],
+--			[[*-----------*                                                ]],
+--			[[    o             boing         boing         boing          ]],
+--			[[     o  e-e           . - .         . - .         . - .      ]],
+--			[[       (\_/)\       '       `.   ,'       `.   ,'       .    ]],
+--			[[        `-'\ `--.___,         . .           . .          .   ]],
+--			[[           '\( ,_.-'                                      .  ]],
+--			[[              \\               "             "            "  ]],
+--			[[              ^'^                                            ]],
+--		}
+--
+--		-- Set menu
+--		dashboard.section.buttons.val = {
+--			dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+--    --	dashboard.button( "LDR fr", "   Recent file", ":Telescope oldfiles<CR>"),
+--    	dashboard.button( "LDR fr", "  Recent file", ":Telescope oldfiles<CR>"),
+--    	-- dashboard.button( "f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+--    	dashboard.button( "LDR ff", "󰈞  Find file", ":Telescope find_files<CR>"),
+--    	dashboard.button( "q", "󰅚  Quit NVIM", ":qa<CR>"),
+--		}
+--
+--		alpha.setup(dashboard.opts)
+--	end,
+--
+--
+--}
 
-		-- Set Header
-		dashboard.section.header.val = {
-			[[*-----------*                                                ]],
-			[[| Look what?|                                                ]],
-			[[*-----------*                                                ]],
-			[[    o             boing         boing         boing          ]],
-			[[     o  e-e           . - .         . - .         . - .      ]],
-			[[       (\_/)\       '       `.   ,'       `.   ,'       .    ]],
-			[[        `-'\ `--.___,         . .           . .          .   ]],
-			[[           '\( ,_.-'                                      .  ]],
-			[[              \\               "             "            "  ]],
-			[[              ^'^                                            ]],
-		}
-
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-    --	dashboard.button( "LDR fr", "   Recent file", ":Telescope oldfiles<CR>"),
-    	dashboard.button( "LDR fr", "  Recent file", ":Telescope oldfiles<CR>"),
-    	-- dashboard.button( "f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
-    	dashboard.button( "LDR ff", "󰈞  Find file", ":Telescope find_files<CR>"),
-    	dashboard.button( "q", "󰅚  Quit NVIM", ":qa<CR>"),
-		}
-
-		alpha.setup(dashboard.opts)
-	end,
 
 
-}
