76c23458c68ceefca7068575579650c3b50ed5ba
zelong <zelong.guo@outlook.com>
2024-09-29 20:25:20 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,64 @@
 --  vim-colorschemes
-return{
-  "flazz/vim-colorschemes",
-  lazy = false,     -- disable lazy loading
-  priority = 1000,  -- high priority
+--  Other light themes:
+-- sainnhe/edge, tokyonight.nvim, apprentice.nvim, everforest-nvim, sainnhe/everforest
+-- ishan9299/nvim-solarized-lua, savq/melange-nvim, Th3Whit3Wolf/space-nvim, Th3Whit3Wolf/one-nvim
+return {
+  "NTBBloodbath/doom-one.nvim",
+  layzy = false,     -- disable lazy loading
+  priority = 1000,   -- high priority
+
+  setup = function()
+    -- Add color to cursor
+    vim.g.doom_one_cursor_coloring = false
+    -- Set :terminal colors
+    vim.g.doom_one_terminal_colors = true
+    -- Enable italic comments
+    vim.g.doom_one_italic_comments = false
+    -- Enable TS support
+    vim.g.doom_one_enable_treesitter = true
+    -- Color whole diagnostic text or only underline
+    vim.g.doom_one_diagnostics_text_color = false
+    -- Enable transparent background
+    vim.g.doom_one_transparent_background = false
+
+    -- Pumblend transparency
+    vim.g.doom_one_pumblend_enable = false
+    vim.g.doom_one_pumblend_transparency = 20
+ 
+    -- Plugins integration
+    vim.g.doom_one_plugin_neorg = false
+    vim.g.doom_one_plugin_barbar = false
+    vim.g.doom_one_plugin_telescope = true
+    vim.g.doom_one_plugin_neogit = true
+    vim.g.doom_one_plugin_nvim_tree = true
+    vim.g.doom_one_plugin_dashboard = true
+    vim.g.doom_one_plugin_startify = true
+    vim.g.doom_one_plugin_whichkey = true
+    vim.g.doom_one_plugin_indent_blankline = true
+    vim.g.doom_one_plugin_vim_illuminate = true
+    vim.g.doom_one_plugin_lspsaga = false
+	end,
+ 
   config = function()
     vim.opt.background = "light",
-    vim.cmd("colorscheme Atelier_ForestLight")
-  end,
+    vim.cmd("colorscheme doom-one")
+   end,
+
 }
 
--- The Light schemes
+
+
+-- return{
+--   "flazz/vim-colorschemes",
+--   lazy = false,     -- disable lazy loading
+--   priority = 1000,  -- high priority
+--   config = function()
+--     vim.opt.background = "light",
+--     vim.cmd("colorscheme Atelier_ForestLight")
+--   end,
+-- }
+--
+-- -- The Light schemes
 -- colorscheme solarized8_light_high
 -- colorscheme Atelier_DuneLight
 -- colorscheme Atelier_EstuaryLight
