d8e90b4632f333bbb4812cca8eba7b1484e848be
zelong <zelong.guo@outlook.com>
2024-10-10 20:06:28 +0200

    add markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,27 @@
 return {
 
-  -- Makrdown table mode
-  'Kicamon/markdown-table-mode.nvim',
-  ft = {"markdown"},
+   -- Makrdown table mode
+  {
+    'Kicamon/markdown-table-mode.nvim',
+    ft = {"markdown"},
 
-  config = function()
-    -- all defaults settings
-    require('markdown-table-mode').setup()
-  end
+    config = function()
+      -- all defaults settings
+      require('markdown-table-mode').setup()
+    end
+  },
+
+  -- Markdown Preview
+  -- {
+	-- 	"instant-markdown/vim-instant-markdown",
+	-- 	ft = { "markdown" },
+	-- 	build = "yarn install",
+	-- 	config = function()
+  --     vim.g.instant_markdown_slow = 0
+	--     vim.g.instant_markdown_autostart = 0
+	-- 	end,
+	-- },
+
+
 }
 
