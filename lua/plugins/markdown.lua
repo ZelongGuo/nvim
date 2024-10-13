a673465953298c604d74bdf29faf313b2fc71510
zelong <zelong.guo@outlook.com>
2024-10-13 13:04:08 +0200

    update markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -21,6 +21,23 @@ return {
 	--     vim.g.instant_markdown_autostart = 0
 	-- 	end,
 	-- },
+  --
+
+  -- install with yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_theme = { "light" }
+
+      vim.keymap.set('n', 'R', ':MarkdownPreview<CR>', { noremap = false, silent = true })
+      vim.keymap.set('n', 'E', ':MarkdownPreviewStop<CR>', { noremap = false, silent = true })
+      -- vim.keymap.set('n', '<C-p>', '<Plug>MarkdownPreviewToggle', { noremap = false, silent = true })
+    end,
+    ft = { "markdown" },
+  },
 
 
 }
