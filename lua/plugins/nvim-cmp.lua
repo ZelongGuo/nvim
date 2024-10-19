188021c2b9411bf7c8891893207abbb947eee41e
zelong <zelong.guo@outlook.com>
2024-10-19 22:58:15 +0200

    update

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,8 +11,9 @@ local M = {
         },
         "saadparwaiz1/cmp_luasnip",     -- for autocompletion
         "rafamadriz/friendly-snippets", -- useful snippets
-        "onsails/lspkind.nvim",         -- vs-code like pictograms
+        "onsails/lspkind.nvim",         -- vs-code like pictograms/icons
         "f3fora/cmp-spell",             -- English spell
+        -- cmp-latex-symbols
     },
 }
 
@@ -28,7 +29,7 @@ function M.config()
         completion = {
             completeopt = "menu,menuone,preview,noselect",
         },
-        snippet = { -- configure how nvim-cmp interacts with snippet engine
+        snippet = {  -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
             luasnip.lsp_expand(args.body)
         end,
@@ -37,22 +38,21 @@ function M.config()
         mapping = cmp.mapping.preset.insert({
             -- TODO ...
             ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-            ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+            ["<Tab>"] = cmp.mapping.select_next_item(),   -- next suggestion
             ["<Down>"] = cmp.mapping.scroll_docs(-4),
             ["<Up>"] = cmp.mapping.scroll_docs(4),
-            ["<C-o>"] = cmp.mapping.complete(),         -- show completion suggestions
-            ["<C-c>"] = cmp.mapping.abort(),            -- close completion window
+            ["<C-o>"] = cmp.mapping.complete(),           -- show completion suggestions
+            ["<C-c>"] = cmp.mapping.abort(),              -- close completion window
             ["<CR>"]  = cmp.mapping.confirm({ select = true }),
         }),
 
         sources = cmp.config.sources({ -- sources for autocompletion
-            { name = "nvim_lsp"},
-            { name = "luasnip" }, -- snippets
-            { name = "buffer" },  -- text within current buffer
-            { name = "path" },    -- file system paths
+            { name = "nvim_lsp", priority = 1000},
+            { name = "luasnip",  priority = 900 },    -- snippets
+            { name = "buffer",   priority = 800 },    -- text within current buffer
+            { name = "path",     priority = 700 },    -- file system paths
             {
-              name = 'spell',
-              option = {
+              name = 'spell', priority = 500, option = {
                 enable_in_context = function()
                   return require('cmp.config.context').in_treesitter_capture('spell')
                 end,
@@ -76,77 +76,3 @@ end
 
 return M
 
---1 return {
---1   "hrsh7th/nvim-cmp",
---1   -- event = { "InsertEnter", "CmdlineEnter" },
---1   event = { "InsertEnter" },
---1   dependencies = {
---1     "hrsh7th/cmp-buffer",  -- source for text in buffer
---1     "hrsh7th/cmp-path",    -- source for file system paths
---1     {
---1       "L3MON4D3/LuaSnip",  -- snippet engine
---1       -- follow latest release.
---1       version = "v2.*",    -- Replace <CurrentMajor> by the latest released major (first number of latest release)
---1       -- install jsregexp (optional!).
---1       build = "make install_jsregexp",
---1     },
---1     "saadparwaiz1/cmp_luasnip",     -- for autocompletion
---1     "rafamadriz/friendly-snippets", -- useful snippets
---1     "onsails/lspkind.nvim",         -- vs-code like pictograms
---1     {
---1       "f3fora/cmp-spell",             -- English spell
---1     }
---1   },
---1   config = function()
---1     local cmp = require("cmp")
---1 
---1     local luasnip = require("luasnip")
---1 
---1     local lspkind = require("lspkind")
---1 
---1     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
---1     require("luasnip.loaders.from_vscode").lazy_load()
---1 
---1     cmp.setup({
---1       completion = {
---1         completeopt = "menu,menuone,preview,noselect",
---1       },
---1       snippet = { -- configure how nvim-cmp interacts with snippet engine
---1         expand = function(args)
---1           luasnip.lsp_expand(args.body)
---1         end,
---1       },
---1       mapping = cmp.mapping.preset.insert({
---1         -- TODO ...
---1         ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
---1         ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
---1         ["<Down>"] = cmp.mapping.scroll_docs(-4),
---1         ["<Up>"] = cmp.mapping.scroll_docs(4),
---1         ["<C-o>"] = cmp.mapping.complete(),         -- show completion suggestions
---1         ["<C-c>"] = cmp.mapping.abort(),            -- close completion window
---1         ["<CR>"]  = cmp.mapping.confirm({ select = true }),
---1       }),
---1       -- sources for autocompletion
---1       sources = cmp.config.sources({
---1         { name = "nvim_lsp"},
---1         { name = "luasnip" }, -- snippets
---1         { name = "buffer" },  -- text within current buffer
---1         { name = "path" },    -- file system paths
---1       }),
---1 
---1       window = {  -- window box better for light scheme
---1         completion = cmp.config.window.bordered(),
---1         documentation = cmp.config.window.bordered(),
---1       },
---1 
---1       -- configure lspkind for vs-code like pictograms in completion menu
---1       formatting = {
---1         format = lspkind.cmp_format({
---1           maxwidth = 50,
---1           ellipsis_char = "...",
---1         }),
---1       },
---1     })
---1   end,
---1 }
---1 
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -3,65 +3,65 @@ return {
   -- Run `:InspectTree` to check what the AST look like.
   -- To compare view without highlight, try to open a file with `nvim <filename> --noplugin`
   -- Tree-sitter itself does not provide highlight, it only provide parser; while the genuine highlight providing is nvim-treesitter which is absolutely based on tree-sitter.
+  --
+    "nvim-treesitter/nvim-treesitter",
+
+    lazy = false,
+    priority = 1000,
+    build = ":TSUpdate",
  
-  "nvim-treesitter/nvim-treesitter",
+    config = function()
+        vim.opt.smartindent = false,
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            sync_install = false,
+            indent = { enable = true, disable = { "python" } },
+            ensure_installed = {
+                "markdown",
+                "html",
+                "latex",
+                "fortran",
+                "c",
+                "bash",
+                "bibtex",
+                "lua",
+                "matlab",
+                "vim",
+                "python",
+            },
 
-  klazy = false,
-  priority = 1000,
-  build = ":TSUpdate",
- 
-  config = function()
-    vim.opt.smartindent = false,
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			sync_install = false,
-			ensure_installed = {
-				"markdown",
-				"html",
-				"latex",
-				"fortran",
-				"c",
-				"bash",
-				"bibtex",
-				"lua",
-				"matlab",
-				"vim",
-				"python",
-			},
+            ignore_install = {},
+            highlight = {
+                enable = true,
+                -- disable = {}, -- list of language that will be disabled
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                additional_vim_regex_highlighting = false,
+            },
 
-      ignore_install = {},
+            -- indent = {
+            -- 	enable = true,
+            -- 	disable = function(lang, bufnr)
+            -- 	    local disallowed_filetypes = { "yaml", "dart" }
+            -- 	    return vim.tbl_contains(disallowed_filetypes, lang)
+            -- 	end,
+            -- },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection    = "<CR>",
+                    node_incremental  = "<CR>",
+                    node_decremental  = "<BS>",
+                    scope_incremental = "<TAB>",
+                },
+             },
 
-			highlight = {
-				enable = true,
-				-- disable = {}, -- list of language that will be disabled
-        disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
+        })
     end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        additional_vim_regex_highlighting = false,
-			},
-			-- indent = {
-			-- 	enable = true,
-			-- 	disable = function(lang, bufnr)
-			-- 		local disallowed_filetypes = { "yaml", "dart" }
-			-- 		return vim.tbl_contains(disallowed_filetypes, lang)
-			-- 	end,
-			-- },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection    = "<CR>",
-					node_incremental  = "<CR>",
-					node_decremental  = "<BS>",
-					scope_incremental = "<TAB>",
-				},
-			},
-
-		})
-	end,
 }
