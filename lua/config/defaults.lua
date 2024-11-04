311416ba65682de350be44e1cd1559cafe22c1c1
zelong <zelong.guo@outlook.com>
2024-11-04 14:59:30 +0100

    update

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -17,8 +17,7 @@ if vim.fn.has("termguicolors") == 1 then
     vim.cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
     vim.cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
 end
---]]
-
+]]--
 
 -- Disable Compatibility of Old Vim Versions
 vim.opt.compatible = false
