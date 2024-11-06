01f3d703e1ca71e3d37f0946f8255787d7b93a21
zelong <zelong.guo@outlook.com>
2024-11-06 20:40:10 +0100

    update indent plugin

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,82 +1,45 @@
 return {
-  "nvimdev/indentmini.nvim",
+    'lukas-reineke/indent-blankline.nvim', -- it could be integrated by many colorscheme
+    main = 'ibl',
 
-  config = function()
-    require("indentmini").setup()
+    config = function()
+        --1 -- Define custom highlight groups for each indent level
+        --1 vim.api.nvim_set_hl(0, "ScopeHighlight1", { fg = "seagreen" })
 
-    -- Dynamical chunk color with the vim theme
-    -- echo g:colors_name
-    if vim.g.colors_name == "doom-one" then  -- light theme
-      vim.cmd.highlight('IndentLine guifg=gray80')
-      vim.cmd.highlight('IndentLineCurrent guifg=seagreen')
-    else -- dark theme
-      vim.cmd.highlight('IndentLine guifg=gray80')
-      vim.cmd.highlight('IndentLineCurrent guifg=#806d9c')
-    end
+        -- Register hooks for scope highlighting
+        local hooks = require("ibl.hooks")
+        hooks.register(
+            hooks.type.SCOPE_HIGHLIGHT,
+            hooks.builtin.scope_highlight_from_extmark
+        )
 
-  end,
+        -- Setup ibl with a list of scope highlight groups
+        require("ibl").setup({
+            indent = {
+                -- │ ┃ ╎ ╏ ┆ ┇ ┊ ┋
+                char = '┆',
+            },
+            whitespace = {
+                remove_blankline_trail = false,
+            },
+            exclude = {
+                filetypes = {
+                    "help", "alpha", "dashboard", "nvim-tree",
+                    "Trouble", "trouble", "lazy", "mason",
+                    "notify", "toggleterm", "lazyterm",
+                },
+            },
+            scope = { 
+                enabled = true,
+                char = '┇',
+                show_start = true,
+                show_end = true,
+                --2 highlight = { 
+                --2     "ScopeHighlight1", 
+                --2 } 
+            }
+        })
+    end,
 }
 
--- TODO: When using hlchunk, the following error threw (particularly with opening telescope.lua ...):
--- Error executing vim.schedule lua callback: ...e/nvim/lazy/hlchunk.nvim/lua/hlchunk/mods/chunk/init.lua:168: attempt to compare nil with number
--- stack traceback:
---         ...e/nvim/lazy/hlchunk.nvim/lua/hlchunk/mods/chunk/init.lua:168: in function 'fn'
---         ...re/nvim/lazy/hlchunk.nvim/lua/hlchunk/utils/loopTask.lua:63: in function 'fn'
---         ...share/nvim/lazy/hlchunk.nvim/lua/hlchunk/utils/timer.lua:16: in function <...share/nvim/lazy/hlchunk.nvim/lua/hlchunk/utils/timer.lua:15>
 
--- return {
---   {
---     "shellRaining/hlchunk.nvim",
--- 
---     init = function()
---       -- Automatically trigger this plugin settings when the cursor moved under the normal and insert modes
---       -- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL", })
--- 
---       -- Dynamical chunk color with the vim theme
---       local cb = function()
---         -- echo g:colors_name
---         if vim.g.colors_name == "doom-one" then  -- light theme
---           return "seagreen" 
---         else -- dark theme
---           return "#806d9c"   -- yellow
---         end
---       end
--- 
---       require('hlchunk').setup({
---         chunk = {
---           enable = true,
---           use_treesitter = true,
---           style = {
---             --{	fg = "#806d9c"},
---             {	fg = cb },
---             {	fg = "red"}, -- if there is error then, the chunk color is red
---           },
---         },
--- 
---         indent = {
---           enable = true,
---           -- chars = { "│", "¦", "┆", "┊", },
---           chars = { "│" },
---           use_treesitter = false,
---           style = {
---             "gray80"
---           }
---         },
--- 
---         blank = {
---           enable = false,
---           priority = 9,
---           chars = {
---             ":",
---           },
---           style = {"black"},
---         },
--- 
---         line_num = {
---         enable = false,
---         use_treesitter = false,
---         },
---       })
---     end
---   },
--- }
