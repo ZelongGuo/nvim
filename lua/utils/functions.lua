4bac43c50b6c7f870699a7301b5d33ec3f33778e
zelong <zelong.guo@outlook.com>
2024-11-08 10:57:58 +0100

    add some costomized functions

--- lua/utils/functions.lua
+++ lua/utils/functions.lua
@@ -10,11 +10,37 @@ if vim.fn.has("autocmd") == 1 then
     })
 end
 
+----------------------------------------------------------------------------------------------------
+
 -- Hightlight on yank
+local highlight_yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = augroup("highlight_yank"),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  group = highlight_yank_group,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+----------------------------------------------------------------------------------------------------
+-- When open split a window (enter a new buffer or new window), opening and setting the LineNr
+-- column aotumatically (default no opening of the LineNr column)
+vim.api.nvim_create_augroup("RelativeNumberToggle", { clear = true })
+vim.api.nvim_create_autocmd({"BufWinEnter", "WinEnter"}, {
+    group = "RelativeNumberToggle",
+    pattern = "*",
+    callback = function()
+        if vim.fn.win_gettype() ~= "popup" then
+            vim.opt.relativenumber = true
+        end
+    end,
+})
+
+-- Close LineNr when leave
+vim.api.nvim_create_autocmd("WinLeave", {
+    group = "RelativeNumberToggle",
+    pattern = "*",
+    callback = function()
+        vim.opt.relativenumber = false
+    end,
 })
 
