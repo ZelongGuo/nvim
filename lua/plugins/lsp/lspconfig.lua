c8dc6c6d456c9f30a3be81c3763100fe8aa6a01b
zelong <zelong.guo@outlook.com>
2024-11-13 20:20:55 +0100

    update

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,12 +1,11 @@
--- Language Server Protocol: a protocol used for interacting (e.g., jumping, completion etc) between EDITOR PLUGINS and the LANGUAGE SERVER.
--- Developing Tools -> LSP -> Languages
---
 local M = {
     "neovim/nvim-lspconfig",
-    -- event = "VeryLazy",
+    -- event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig",
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig" },
+        { "j-hui/fidget.nvim", },
+        -- { "hrsh7th/cmp-nvim-lsp" },
     },
 }
 
@@ -14,9 +13,7 @@ function M.config()
 
     -------------------------------- NVIM LSPCONFIG ---------------------------------------------
     -- vim.lsp.set_log_level("error")
-    vim.lsp.set_log_level("off")
-    -- setup lsp
-    -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for supported languages
+    -- vim.lsp.set_log_level("off")
 
     ---------------- GLOBAL MAPPINGS ----------------
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -30,50 +27,115 @@ function M.config()
     -- diagnostic
     vim.diagnostic.config({ virtual_text = true, underline = false })
 
-    ---------------- BUFFER MAPPINGS ----------------
-    -- Use LspAttach autocommand to only map the following keys
-    -- after the language server attaches to the current buffer
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      callback = function(ev)
+    --------------------------------------------------------------------------------
+    ---
+    -- TODO: Learn more about vim.diagnostic.config and change the signs ...
+    ---
+    --------------------------------------------------------------------------------
+
+    -- SERVERS settings
+    -- For available parameters see vim_lsp_start()
+    local servers = {
+        lua_ls = {
+            -- cmd = {},
+            -- name = {},
+            -- capabilities = {},
+            -- on_attach = on_attach,
+            settings = { -- How to configure it should see language server official doduments (e.g., lua_ls)
+                Lua = {
+                    -- diagnostics = { globals = { 'vim' }, }, -- global virable of vim for lua to prevent warnings
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
+                },
+            },
+        },
+        pyright = {},
+        bashls = {},
+        marksman = {},
+        ltex = {},
+        clangd = {},
+    }
+
+    local on_attach = function(_, bufnr)
         -- Enable completion triggered by <c-x><c-o>
-        -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = 'LSP: ' .. desc
+            end
 
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local buffer = ev.buf
-        local opts = { buffer = buffer }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- [G]oto [D]eclaration
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)  -- [G]oto [D]efinition
-        vim.keymap.set("n", "gk", vim.lsp.buf.hover, opts)       -- Hover Documentation
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)  -- [G]oto [I]mplementation
-        vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, opts)  -- Signature Documentation
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)  -- [W]orkspace [A]dd Folder'
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts) -- [W]orkspace [R]emove Folder
-        vim.keymap.set("n", "<leader>wl", function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders())) -- [W]orkspace [L]ist Folders 
-        end, opts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)  -- Type [D]efinition
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)  -- [R]e[n]ame
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- [C]ode [A]ction'
-        vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts) -- [G]oto [R]eferences')
-        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set({ "n", "v" }, "<leader>cf", function()
-          vim.lsp.buf.format({ async = true })
-        end, opts)
-
-        -- inlay hints
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if
-            client.supports_method("textDocument/inlayHint") or client.server_capabilities.inlayHintProvider
-        then
-          if vim.lsp.inlay_hint then -- I don't know why somehow vim.lsp.inlay_hint is nil, so do this check befre enable inlayhint
-            vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
-          end
+            vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
         end
-      end,
-    })
 
+        -- Check out telescope LSP picker
+        nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        nmap('gd', require "telescope.builtin".lsp_definitions, '[G]oto [D]efinition')
+        nmap('gk', vim.lsp.buf.hover, 'Hover Documentation')
+        -- nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+        nmap('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        nmap('gh', vim.lsp.buf.signature_help, 'Signature Documentation')
+        nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+        nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+        nmap('<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, '[W]orkspace [L]ist Folders')
+        nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+        nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        nmap('<leader>da', require "telescope.builtin".diagnostics, '[D]i[A]gnostics')
+        nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        -- nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+        nmap("<space>cf", function()
+            vim.lsp.buf.format { async = true }
+        end, "[F]ormat code")
+    end
+
+    --1 ---------------- BUFFER MAPPINGS ----------------
+    --1 -- Use LspAttach autocommand to only map the following keys after the language server attaches to the current buffer
+    --1 vim.api.nvim_create_autocmd("LspAttach", {
+    --1     desc = "LSP Actions ",
+    --1     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    --1     callback = function(event)
+    --1         -- Enable completion triggered by <c-x><c-o>
+    --1         -- vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    --1         -- Buffer local mappings.
+    --1         -- See `:help vim.lsp.*` for documentation on any of the below functions
+    --1         local buffer = event.buf
+    --1         local opts = { buffer = buffer, noremap = true, nowait = true }
+    --1         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)                     -- [G]oto [D]eclaration
+    --1         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)                      -- [G]oto [D]efinition
+    --1         vim.keymap.set("n", "gk", vim.lsp.buf.hover, opts)                           -- Hover Documentation
+    --1         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)                  -- [G]oto [I]mplementation
+    --1         vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, opts)                  -- Signature Documentation
+    --1         vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)    -- [W]orkspace [A]dd Folder'
+    --1         vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts) -- [W]orkspace [R]emove Folder
+    --1         vim.keymap.set("n", "<leader>wl", function()
+    --1             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))                 -- [W]orkspace [L]ist Folders
+    --1         end, opts)
+    --1         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)          -- Type [D]efinition
+    --1         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)                  -- [R]e[n]ame
+    --1         vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)    -- [C]ode [A]ction'
+    --1         vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts) -- [G]oto [R]eferences')
+    --1         -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    --1         vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+    --1             vim.lsp.buf.format({ async = true })
+    --1         end, opts)
+
+    --1         -- inlay hints
+    --1         local client = vim.lsp.get_client_by_id(event.data.client_id)
+    --1         if
+    --1             client.supports_method("textDocument/inlayHint") or client.server_capabilities.inlayHintProvider
+    --1         then
+    --1             if vim.lsp.inlay_hint then -- I don't know why somehow vim.lsp.inlay_hint is nil, so do this check befre enable inlayhint
+    --1                 vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
+    --1             end
+    --1         end
+    --1     end,
+    --1 })
+
+    -------------------------------- FIDGET SETUPS  ---------------------------------------------
+    require("fidget").setup({})
     -------------------------------- MASON SETUPS ---------------------------------------------
     vim.keymap.set('n', '<leader>ma', ":Mason<CR>", { desc = "open Mason window" })
 
@@ -121,104 +183,33 @@ function M.config()
     })
 
     ---------------------------- MASON LSPCONFIG SETUPS -------------------------------------------
-    -- SERVERS SETTINGS
-    local servers = {
-        lua_ls = {
-            Lua = {
-                diagnostics = { globals = { 'vim' }, },  -- global virable of vim for lua to prevent warnings
-                workspace = { checkThirdParty = false },
-                telemetry = { enable = false },
-            },
-        },
-        -- pyright = {},
-        -- jsonls = {},
-        -- marksman = {},
-        -- volar = {},
-        -- dockerls = {},
-        -- docker_compose_language_service = {},
-        -- bashls = {},
-        -- ocamllsp = {},
-        -- taplo = {},
-        -- ruff_lsp = {},
-    }
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     require("mason-lspconfig").setup({
-        ensure_installed = vim.tbl_keys(servers),
-        handlers = {
-            function(server_name) -- default handler (optional)
-                require("lspconfig")[server_name].setup {
-                    settings = servers[server_name],
-                    -- on_attach = on_attach,
-                }
-            end,
-        }
+        ensure_installed = vim.tbl_keys(servers), -- servers list of automatic installation
+        --handlers = {
+        --    function(server_name) -- default handler (optional)
+        --        require("lspconfig")[server_name].setup {
+        --            settings = servers[server_name],
+        --            -- on_attach = on_attach,
+        --        }
+        --    end,
+        --}
         -- -- auto-install configured servers (with lspconfig)
         -- automatic_installation = true, -- not the same as ensure_installed
     })
 
+    for server, configuration in pairs(servers) do
+        require("lspconfig")[server].setup(
+            vim.tbl_deep_extend("keep",
+                {
+                    on_attach = on_attach,
+                    capabilities = capabilities
+                },
+                configuration
+            )
+        )
+    end
 end
 
 return M
-
-
--- config = function()
---     local servers = {
---         lua_ls = {
---             Lua = {
---                 workspace = { checkThirdParty = false },
---                 telemetry = { enable = false },
---             },
---         },
---         pyright = {},
---         jsonls = {},
---         marksman = {},
---         volar = {},
---         dockerls = {},
---         docker_compose_language_service = {},
---         bashls = {},
---         ocamllsp = {},
---         taplo = {},
---         ruff_lsp = {},
---     }
---     local on_attach = function(_, bufnr)
---         -- Enable completion triggered by <c-x><c-o>
---         local nmap = function(keys, func, desc)
---             if desc then
---                 desc = 'LSP: ' .. desc
---             end
-
---             vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
---         end
-
---         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
---         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
---         nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
---         nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
---         nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
---         nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
---         nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
---         nmap('<leader>wl', function()
---             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---         end, '[W]orkspace [L]ist Folders')
---         nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
---         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
---         nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
---         -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
---         nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
---         nmap("<space>f", function()
---             vim.lsp.buf.format { async = true }
---         end, "[F]ormat code")
---     end
---     require("mason").setup()
---     require("mason-lspconfig").setup({
---         ensure_installed = vim.tbl_keys(servers),
---         handlers = {
---             function(server_name) -- default handler (optional)
---                 require("lspconfig")[server_name].setup {
---                     settings = servers[server_name],
---                     on_attach = on_attach,
---                 }
---             end,
---         }
---     })
--- end
