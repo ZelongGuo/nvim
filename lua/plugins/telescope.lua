4841968648c00ff2350f102ecfb4a279e20b78ee
zelong <zelong.guo@outlook.com>
2024-11-16 22:23:17 +0100

    add a function to live grep strings in the git rep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,10 @@ local M = {
     -- brew install ripgrep
     -- brew install fd
     -- ore details could be found either in telescope or ripgrep
-    'nvim-telescope/telescope.nvim', 	-- tag = '0.1.8',
+    'nvim-telescope/telescope.nvim', -- tag = '0.1.8',
     event = "VeryLazy",
     dependencies = {
-        {'nvim-lua/plenary.nvim', lazy = true },
+        { 'nvim-lua/plenary.nvim', lazy = true },
         {
             "LukasPietzschmann/telescope-tabs",
             -- lazy = true,
@@ -45,8 +45,8 @@ function M.config()
                     -- result_width = 0.5, -- define preview window half of the total window size
                     preview_cutoff = 30, -- show 30 characters in the preview window
                 },
-                width = { padding = 8 },  -- setting the telescope window width
-                height = {padding = 3}, -- setting the telescope window hight
+                width = { padding = 8 }, -- setting the telescope window width
+                height = { padding = 3 }, -- setting the telescope window hight
             },
 
             -- Mappings Within Telescope for Normal and Insert Mode
@@ -61,35 +61,35 @@ function M.config()
                     ["<C-l>"]  = actions.preview_scrolling_down,
                     ["C-c"]    = actions.close,
                     ["C-t"]    = actions.select_tab,
-                    ["<CR>"]   = actions.select_default,    -- default will overwrite current only tab
+                    ["<CR>"]   = actions.select_default, -- default will overwrite current only tab
                     -- open the file with a new vertical buffer in current tab, here we only set the vertical split, if you want a horizontal split, using default key maps
-                    ["<C-s>"]   = actions.select_vertical,
+                    ["<C-s>"]  = actions.select_vertical,
                 },
 
                 n = {
-                    ["i"]      = actions.move_selection_previous,
-                    ["k"]      = actions.move_selection_next,
-                    ["j"]      = actions.preview_scrolling_up,
-                    ["l"]      = actions.preview_scrolling_down,
-                    ["gg"]     = actions.move_to_top,
-                    ["G"]      = actions.move_to_bottom,
+                    ["i"]     = actions.move_selection_previous,
+                    ["k"]     = actions.move_selection_next,
+                    ["j"]     = actions.preview_scrolling_up,
+                    ["l"]     = actions.preview_scrolling_down,
+                    ["gg"]    = actions.move_to_top,
+                    ["G"]     = actions.move_to_bottom,
                     -- ["<PageUp>"]   = actions.results_scrolling_up,
                     -- ["<PageDown>"] = actions.results_scrolling_down,
 
-                    ["<esc>"]  = actions.close,
-                    ["?"]      = actions.which_key,
-                    ["C-t"]    = actions.select_tab,
-                    ["<CR>"]   = actions.select_default,
-                    ["<C-s>"]   = actions.select_vertical,
+                    ["<esc>"] = actions.close,
+                    ["?"]     = actions.which_key,
+                    ["C-t"]   = actions.select_tab,
+                    ["<CR>"]  = actions.select_default,
+                    ["<C-s>"] = actions.select_vertical,
                 },
             },
         },
 
         pickers = {
             colorscheme = {
-                enable_preview = true,   -- preview window
+                enable_preview = true,  -- preview window
                 ignore_builtins = true, -- ignore the default colorschemes or not
-             },
+            },
         },
 
     })
@@ -99,30 +99,46 @@ function M.config()
     -- Define my-fd function to set the work directory as Git root directory
     -- You can also use some plugins like "airblade/vim-rooter" to set the root directory
     local my_fd = function()
-      local opts = {}
-      local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        local opts = {}
+        local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        -- local git_root = string.gsub(vim.fn.systemlist("git rev-parse --show-toplevel")[1] or "", "\n", "")
 
-      if git_root and git_root ~= "" then
-        opts.cwd = git_root  -- Set the work directory to Git root directory
-      else
-        opts.cwd = vim.fn.getcwd()  -- If it's not Git repository, then use current work directory cwd
-      end
+        if git_root and git_root ~= "" then
+            opts.cwd = git_root    -- Set the work directory to Git root directory
+        else
+            opts.cwd = vim.fn.getcwd() -- If it's not Git repository, then use current work directory cwd
+        end
 
-      builtin.find_files(opts)  -- invoke Telescope to search files
+        builtin.find_files(opts) -- invoke Telescope to search files
     end
 
-    vim.keymap.set('n', '<leader>ff', my_fd, { noremap = true, nowait = true, desc = 'Telescope find files in Git root or cwd' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { noremap = true, nowait = true, desc = 'Telescope live grep finding strings in open buffers' })
+    -- Define a function to set the working directory to the Git root for live grep
+    local my_live_grep = function()
+        local opts = {}
+        local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+
+        if git_root and git_root ~= "" then
+            opts.cwd = git_root -- Set the working directory to Git root
+        else
+            opts.cwd = vim.fn.getcwd() -- If it's not a Git repository, use the current working directory
+        end
+
+        builtin.live_grep(opts) -- Invoke Telescope live_grep with the updated options
+    end
+
+    vim.keymap.set('n', '<leader>ff', my_fd,
+        { noremap = true, nowait = true, desc = 'Telescope find files in Git root or cwd' })
+    vim.keymap.set('n', '<leader>fg', my_live_grep,
+        { noremap = true, nowait = true, desc = 'Telescope live grep finding strings in open buffers' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { noremap = true, nowait = true, desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { noremap = true, nowait = true, desc = 'Telescope help tags' })
-    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { noremap = true, nowait = true, desc = 'Lists previously open files' })
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles,
+        { noremap = true, nowait = true, desc = 'Lists previously open files' })
     vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { noremap = true, nowait = true, desc = 'Change colorscheme' })
 
     -- External Plugins
     -- telescope.load_extension('neoclip')  -- neoclip should be loaded
     vim.keymap.set('n', '<leader>fy', ":Telescope neoclip<CR>", { desc = 'open neoclip plugin for yank and macro' })
-
 end
 
 return M
-
