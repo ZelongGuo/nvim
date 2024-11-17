ebbf172bcf1f0524b627b34b92fcae9dbe012ebb
zelong <zelong.guo@outlook.com>
2024-11-17 20:33:20 +0100

    add new diagnostics signs

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -34,10 +34,10 @@ function M.config()
         float = { border = "rounded", source = "always", },
         signs = {
             text = {
-                [vim.diagnostic.severity.ERROR] = '✘', -- ""
-                [vim.diagnostic.severity.WARN] = '▲', -- ""
-                [vim.diagnostic.severity.HINT] = '⚑', -- ""
-                [vim.diagnostic.severity.INFO] = '»', -- "  " 󰍦  
+                [vim.diagnostic.severity.ERROR] = '', -- ""     ✘
+                [vim.diagnostic.severity.WARN] = '', -- "" ▲ 󰲉
+                [vim.diagnostic.severity.HINT] = '', -- ""  ⚑
+                [vim.diagnostic.severity.INFO] = '»', -- "  " 󰍦     󰌵 󰛩 󰍨
             },
             -- linehl = {
             --     [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
