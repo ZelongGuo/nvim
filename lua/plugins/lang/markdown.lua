3a3a10cce12b240d94fb594b75726798a7ac04bc
zelong <zelong.guo@outlook.com>
2024-11-18 15:00:49 +0100

    add markdown TOC

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,44 +1,60 @@
 return {
 
-   -- Makrdown table mode
-  {
-    'Kicamon/markdown-table-mode.nvim',
-    ft = {"markdown"},
+    -- Makrdown table mode
+    {
+        'Kicamon/markdown-table-mode.nvim',
+        ft = { "markdown" },
 
-    config = function()
-      -- all defaults settings
-      require('markdown-table-mode').setup()
-    end
-  },
+        config = function()
+            -- all defaults settings
+            require('markdown-table-mode').setup()
+        end
+    },
 
-  -- Markdown Preview
-  -- {
-	-- 	"instant-markdown/vim-instant-markdown",
-	-- 	ft = { "markdown" },
-	-- 	build = "yarn install",
-	-- 	config = function()
-  --     vim.g.instant_markdown_slow = 0
-	--     vim.g.instant_markdown_autostart = 0
-	-- 	end,
-	-- },
-  --
+    -- Markdown Preview
+    -- {
+    -- 	"instant-markdown/vim-instant-markdown",
+    -- 	ft = { "markdown" },
+    -- 	build = "yarn install",
+    -- 	config = function()
+    --     vim.g.instant_markdown_slow = 0
+    --     vim.g.instant_markdown_autostart = 0
+    -- 	end,
+    -- },
+    --
 
-  -- install with yarn or npm
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    ft = { "markdown" },
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_theme = { "light" }
+    -- install with yarn or npm
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        ft = { "markdown" },
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+            vim.g.mkdp_theme = { "light" }
 
-      vim.keymap.set('n', '<leader>R', ':MarkdownPreview<CR>', { noremap = false, silent = true })
-      -- vim.keymap.set('n', 'E', ':MarkdownPreviewStop<CR>', { noremap = false, silent = true })
-      -- vim.keymap.set('n', '<C-p>', '<Plug>MarkdownPreviewToggle', { noremap = false, silent = true })
-    end,
-  },
+            vim.keymap.set('n', '<leader>R', ':MarkdownPreview<CR>', { noremap = false, silent = true })
+            -- vim.keymap.set('n', 'E', ':MarkdownPreviewStop<CR>', { noremap = false, silent = true })
+            -- vim.keymap.set('n', '<C-p>', '<Plug>MarkdownPreviewToggle', { noremap = false, silent = true })
+        end,
+    },
+
+
+    -- Markdown TOC
+    {
+        'richardbizik/nvim-toc',
+        ft = { "markdown" },
+        config = function()
+            require("nvim-toc").setup({
+                toc_header = "Table of Contents"
+            })
+
+            -- Customized commad mappings
+            vim.api.nvim_create_user_command("GenerateTOC", "TOC", {})
+            vim.api.nvim_create_user_command("GenerateTOCList", "TOCList", {})
+        end
+    },
+
 
 
 }
-
