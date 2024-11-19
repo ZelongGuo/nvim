a73a16a2a0d9ac390221e62ed624281fd1a9b896
zelong <zelong.guo@outlook.com>
2024-11-19 14:41:32 +0100

    add customized movement setttings

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -15,25 +15,7 @@ M.config = function()
     vim.keymap.set('n', '[c', api.goto_context_start)
     vim.keymap.set('n', ']c', api.select_next_context)
 
-    local confirm = function()
-        local menu = api.get_current_dropbar_menu()
-        if not menu then
-            return
-        end
-        local cursor = vim.api.nvim_win_get_cursor(menu.win)
-        local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
-        if component then
-            menu:click_on(component)
-        end
-    end
-
-    local quit_curr = function()
-        local menu = api.get_current_dropbar_menu()
-        if menu then
-            menu:close()
-        end
-    end
-
+    local utils = require("dropbar.utils")
     require("dropbar").setup({
         menu = {
             -- When on, automatically set the cursor to the closest previous/next
@@ -42,45 +24,46 @@ M.config = function()
             quick_navigation = true,
             ---@type table<string, string|function|table<string, string|function>>
             keymaps = {
-                ['<LeftMouse>'] = function()
-                    local menu = api.get_current_dropbar_menu()
+                -- Cursor movement
+                ['j'] = 'h',
+                ['i'] = 'k',
+                ['k'] = 'j',
+                ['h'] = 'i',
+                ['q'] = '<C-w>q',
+                ['<Esc>'] = '<C-w>q',
+                ['jk'] = '<C-w>q',
+
+                ['<CR>'] = function()
+                    local menu = utils.menu.get_current()
                     if not menu then
                         return
                     end
-                    local mouse = vim.fn.getmousepos()
-                    if mouse.winid ~= menu.win then
-                        local parent_menu = api.get_dropbar_menu(mouse.winid)
-                        if parent_menu and parent_menu.sub_menu then
-                            parent_menu.sub_menu:close()
-                        end
-                        if vim.api.nvim_win_is_valid(mouse.winid) then
-                            vim.api.nvim_set_current_win(mouse.winid)
-                        end
-                        return
+                    local cursor = vim.api.nvim_win_get_cursor(menu.win)
+                    local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
+                    if component then
+                        menu:click_on(component, nil, 1, 'l')
                     end
-                    menu:click_at({ mouse.line, mouse.column }, nil, 1, 'l')
                 end,
-                ['<CR>'] = confirm,
-                -- ['i'] = confirm,
-                ['<esc>'] = quit_curr,
-                ['q'] = quit_curr,
-                ['jk'] = quit_curr,
-                ['<MouseMove>'] = function()
-                    local menu = api.get_current_dropbar_menu()
+
+                ['l'] = function()
+                    local menu = utils.menu.get_current()
                     if not menu then
                         return
                     end
-                    local mouse = vim.fn.getmousepos()
-                    if mouse.winid ~= menu.win then
-                        return
+                    local cursor = vim.api.nvim_win_get_cursor(menu.win)
+                    local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
+                    if component then
+                        menu:click_on(component, nil, 1, 'l')
                     end
-                    menu:update_hover_hl({ mouse.line, mouse.column - 1 })
                 end,
             },
         },
+        win_configs = {
+            border = 'rounded',
+            style = 'minimal',
+        },
     })
 end
-
 
 -- ---@class dropbar_configs_t
 -- M.opts = {
@@ -175,5 +158,3 @@ end
 
 return M
 
-
--- --------------------------------------------------------------------------------------------------
