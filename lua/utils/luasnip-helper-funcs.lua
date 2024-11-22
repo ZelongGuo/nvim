c0fe3024c59104e8cc2c10ec4edb03338a428b96
zelong <zelong.guo@outlook.com>
2024-11-22 20:37:12 +0100

    add a new file

---
+++ lua/utils/luasnip-helper-funcs.lua
@@ -0,0 +1,16 @@
+local Func = {}
+
+-- Be sure to explicitly define these LuaSnip node abbreviations!
+local ls = require("luasnip")
+local sn = ls.snippet_node
+local i = ls.insert_node
+
+function Func.get_visual(args, parent)
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+  else
+    return sn(nil, i(1, ''))
+  end
+end
+
+return Func
