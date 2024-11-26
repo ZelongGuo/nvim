8b53681cbd0d5764067640b63c5009f7793aa5d3
zelong <zelong.guo@outlook.com>
2024-11-26 20:44:33 +0100

    add a get_time function

--- lua/utils/luasnip-helper-funcs.lua
+++ lua/utils/luasnip-helper-funcs.lua
@@ -6,11 +6,16 @@ local sn = ls.snippet_node
 local i = ls.insert_node
 
 function Func.get_visual(args, parent)
-  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
-    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
-  else
-    return sn(nil, i(1, ''))
-  end
+    if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+        return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+    else
+        return sn(nil, i(1, ''))
+    end
+end
+
+function Func.get_time()
+    -- return os.date("%Y-%m-%d %H:%M:%S")
+    return os.date("%m%.%d%.%Y %H:%M:%S")
 end
 
 return Func
