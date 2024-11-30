9f491ca910b5b2a32ca45a2fb8e113c18da50575
zelong <zelong.guo@outlook.com>
2024-11-30 22:10:14 +0100

    add ftplugin for c

---
+++ ftplugin/c.lua
@@ -0,0 +1,9 @@
+-- indentation settings
+vim.bo.expandtab   = true
+vim.bo.autoindent  = true
+vim.bo.tabstop     = 4
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth  = 4
+vim.wo.foldmethod  = 'indent'
+vim.wo.foldlevel   = 99
+vim.wo.signcolumn  = 'yes'
