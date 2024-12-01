07ff936ccc501a0eb008d0acc2e00bfc67b447e5
zelong <zelong.guo@outlook.com>
2024-12-01 19:55:46 +0100

    rewrote with lua

---
+++ ftplugin/lua.lua
@@ -0,0 +1,8 @@
+-- indentation settings
+vim.bo.expandtab   = true
+vim.bo.autoindent  = true
+vim.bo.tabstop     = 4
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth  = 4
+
+vim.keymap.set('', '<Leader>r', '<Cmd>Update<CR><Cmd>TermExec cmd="lua %"<CR>')
