a4aac1f101a5b552e0fc8afbc64b261ff24074d9
zelong <zelong.guo@outlook.com>
2024-12-04 11:34:14 +0100

    disable all the defaule mapping and add customized ones

--- lua/plugins/editor/neoscroll.lua
+++ lua/plugins/editor/neoscroll.lua
@@ -12,15 +12,16 @@ return {
             --     '<C-y>', '<C-e>',
             --     'zt', 'zz', 'zb',
             -- },
-            hide_cursor = false,     -- Hide cursor while scrolling
-            stop_eof = true,         -- Stop at <EOF> when scrolling downwards
-            respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+            mappings = {},               -- Disable all the default mappings
+            hide_cursor = false,         -- Hide cursor while scrolling
+            stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+            respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
             cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
             -- easing = 'linear',       -- Default easing function
-            pre_hook = nil,          -- Function to run before the scrolling animation starts
-            post_hook = nil,         -- Function to run after the scrolling animation ends
-            performance_mode = false, -- Disable "Performance Mode" on all buffers.
-            ignored_events = {       -- Events ignored while scrolling
+            pre_hook = nil,              -- Function to run before the scrolling animation starts
+            post_hook = nil,             -- Function to run after the scrolling animation ends
+            performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+            ignored_events = {           -- Events ignored while scrolling
                 'WinScrolled', 'CursorMoved'
             },
         })
@@ -33,11 +34,13 @@ return {
             ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450, easing = 'circular' }) end,
             ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450, easing = 'circular' }) end,
             -- When no value is passed the `easing` option supplied in `setup()` is used
-            ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
-            -- Comment <C-e> because it conflicts with the node choice of the luasnip
+            -- Disable <C-y> and <C-e>
+            -- ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
             -- ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
         }
-        local modes = { 'n', 'v', 'x' }
+        -- local modes = { 'n', 'v', 'x' }
+        -- Only for normal mode avoiding luasnip node choices <C-e>
+        local modes = { 'n', }
         for key, func in pairs(keymap) do
             vim.keymap.set(modes, key, func)
         end
