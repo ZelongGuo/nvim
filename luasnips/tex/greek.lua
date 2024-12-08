37545bd335a39cec8fed2f094945d3a709cdd95a
zelong <zelong.guo@outlook.com>
2024-12-08 19:33:21 +0100

    add snippets for greek letters

---
+++ luasnips/tex/greek.lua
@@ -0,0 +1,140 @@
+-- Return snippet tables
+return
+{
+    s({ trig = ";a", dscr = "α", snippetType = "autosnippet" },
+        {
+            t("\\alpha"),
+        }),
+    s({ trig = ";b", dscr = "β", snippetType = "autosnippet" },
+        {
+            t("\\beta"),
+        }),
+    s({ trig = ";g", dscr = "γ", snippetType = "autosnippet" },
+        {
+            t("\\gamma"),
+        }),
+    s({ trig = ";G", dscr = "Γ", snippetType = "autosnippet" },
+        {
+            t("\\Gamma"),
+        }),
+    s({ trig = ";d", dscr = "δ", snippetType = "autosnippet" },
+        {
+            t("\\delta"),
+        }),
+    s({ trig = ";D", dscr = "Δ", snippetType = "autosnippet" },
+        {
+            t("\\Delta"),
+        }),
+    s({ trig = ";e", dscr = "ϵ", snippetType = "autosnippet" },
+        {
+            t("\\epsilon"),
+        }),
+    s({ trig = ";ve", dscr = "ε", snippetType = "autosnippet" },
+        {
+            t("\\varepsilon"),
+        }),
+    s({ trig = ";z", dscr = "ζ", snippetType = "autosnippet" },
+        {
+            t("\\zeta"),
+        }),
+    s({ trig = ";h",dscr = "η", snippetType = "autosnippet" },
+        {
+            t("\\eta"),
+        }),
+    s({ trig = ";o", dscr = "θ", snippetType = "autosnippet" },
+        {
+            t("\\theta"),
+        }),
+    s({ trig = ";vo", dscr = "ϑ", snippetType = "autosnippet" },
+        {
+            t("\\vartheta"),
+        }),
+    s({ trig = ";O", dscr = "Θ", snippetType = "autosnippet" },
+        {
+            t("\\Theta"),
+        }),
+    s({ trig = ";k", dscr = "κ", snippetType = "autosnippet" },
+        {
+            t("\\kappa"),
+        }),
+    s({ trig = ";l", dscr = "λ", snippetType = "autosnippet" },
+        {
+            t("\\lambda"),
+        }),
+    s({ trig = ";L", dscr = "Λ", snippetType = "autosnippet" },
+        {
+            t("\\Lambda"),
+        }),
+    s({ trig = ";m", dscr = "μ", snippetType = "autosnippet" },
+        {
+            t("\\mu"),
+        }),
+    s({ trig = ";n", dscr = "ν", snippetType = "autosnippet" },
+        {
+            t("\\nu"),
+        }),
+    s({ trig = ";x", dscr = "ξ", snippetType = "autosnippet" },
+        {
+            t("\\xi"),
+        }),
+    s({ trig = ";X", dscr = "Ξ", snippetType = "autosnippet" },
+        {
+            t("\\Xi"),
+        }),
+    s({ trig = ";i", dscr = "π", snippetType = "autosnippet" },
+        {
+            t("\\pi"),
+        }),
+    s({ trig = ";I", dscr = "Π", snippetType = "autosnippet" },
+        {
+            t("\\Pi"),
+        }),
+    s({ trig = ";r", dscr = "ρ", snippetType = "autosnippet" },
+        {
+            t("\\rho"),
+        }),
+    s({ trig = ";s", dscr = "σ", snippetType = "autosnippet" },
+        {
+            t("\\sigma"),
+        }),
+    s({ trig = ";S", dscr = "Σ", snippetType = "autosnippet" },
+        {
+            t("\\Sigma"),
+        }),
+    s({ trig = ";t", dscr = "τ", snippetType = "autosnippet" },
+        {
+            t("\\tau"),
+        }),
+    s({ trig = ";f", dscr = "ϕ", snippetType = "autosnippet" },
+        {
+            t("\\phi"),
+        }),
+    s({ trig = ";vf", dscr = "φ", snippetType = "autosnippet" },
+        {
+            t("\\varphi"),
+        }),
+    s({ trig = ";F", dscr = "Φ", snippetType = "autosnippet" },
+        {
+            t("\\Phi"),
+        }),
+    s({ trig = ";c", dscr = "χ", snippetType = "autosnippet" },
+        {
+            t("\\chi"),
+        }),
+    s({ trig = ";p", dscr = "ψ", snippetType = "autosnippet" },
+        {
+            t("\\psi"),
+        }),
+    s({ trig = ";P", dscr = "Ψ", snippetType = "autosnippet" },
+        {
+            t("\\Psi"),
+        }),
+    s({ trig = ";w", dscr = "ω", snippetType = "autosnippet" },
+        {
+            t("\\omega"),
+        }),
+    s({ trig = ";W", dscr = "Ω", snippetType = "autosnippet" },
+        {
+            t("\\Omega"),
+        }),
+}
