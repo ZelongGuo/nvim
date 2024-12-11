4850a52e0a693fe35ed2c8957758a1b78f07b0d6
zelong <zelong.guo@outlook.com>
2024-12-11 20:03:48 +0100

    change keys

--- lua/plugins/editor/flash.lua
+++ lua/plugins/editor/flash.lua
@@ -13,7 +13,7 @@ return {
     },
     -- stylua: ignore
     keys = {
-        { "/", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "/", mode = { "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { ";", mode = { "n" }, function()
             require("flash").jump({
             search = { mode = "search", max_length = 0 },
@@ -23,6 +23,6 @@ return {
         --   { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+          -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 }
