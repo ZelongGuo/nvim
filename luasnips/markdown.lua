0c93bcd09cbb66c9e245c1b3012e7b198a5f7f5c
zelong <zelong.guo@outlook.com>
2024-12-11 20:06:42 +0100

    change some choice nodes

--- luasnips/markdown.lua
+++ luasnips/markdown.lua
@@ -3,25 +3,17 @@ local get_visual = helper.get_visual
 
 local snippets = {
     --------------------------------------- Pairs -------------------------------------------------
-    s(
-        { trig = ",i", dscr = "Italic", wordTrig = false, priority = 1000, snippetType = "autosnippet" },
-        fmta([[*<>*]], { d(1, get_visual) })
-    ),
+    s({trig = ",i", dscr = "Italic", wordTrig = false, priority = 1000, snippetType = "autosnippet"},
+        fmta([[*<>*]], { d(1, get_visual) })),
 
-    s(
-        { trig = ",b", dscr = "Bold", wordTrig = false, priority = 1000, snippetType = "autosnippet" },
-        fmta([[**<>**]], { d(1, get_visual) })
-    ),
+    s({trig = ",b", dscr = "Bold", wordTrig = false, priority = 1000, snippetType = "autosnippet"},
+        fmta([[**<>**]], { d(1, get_visual) })),
 
-    s(
-        { trig = ",d", dscr = "Delete Line", wordTrig = false, priority = 1000, snippetType = "autosnippet" },
-        fmta([[~~<>~~]], { d(1, get_visual) })
-    ),
+    s({ trig = ",d", dscr = "Delete Line", wordTrig = false, priority = 1000, snippetType = "autosnippet"},
+        fmta([[~~<>~~]], { d(1, get_visual) })),
 
-    s(
-        { trig = ",q", dscr = "Quatation", wordTrig = false, priority = 1000, snippetType = "autosnippet" },
-        fmta([[`<>`]], { d(1, get_visual) })
-    ),
+    s({ trig = ",q", dscr = "Quatation", wordTrig = false, priority = 1000, snippetType = "autosnippet"},
+        fmta([[`<>`]], { d(1, get_visual) })),
 
     --------------------------------------- Others -------------------------------------------------
     -- Colot text, using choice nodes, now deprecated
@@ -52,44 +44,25 @@ local snippets = {
     -- ),
 
     -- Colot text
-    s(
-        { trig = ",c", dscr = "Color Text", snippetType = "autosnippet" },
-            fmta(
-                [[<font size="{}"  color="{}">{}</font>]],
-                { i(1, "3"), i(2, "red"), i(3) },
-                { delimiters = "{}" }
-            )
-    ),
+    s({trig = ",c", dscr = "Color Text", snippetType = "autosnippet" },
+       fmta([[<font size="{}"  color="{}">{}</font>]], { i(1, "3"), i(2, "red"), i(3) }, { delimiters = "{}" })),
     -- Codes
-    s(
-        { trig = ",C", dscr = "Codes block", snippetType = "autosnippet" },
-            fmt(
-                [[
-                    ```<>
-                    <>
-                    ```
-                    ]],
-                { i(1), i(2) },
-                { delimiters = "<>" }
-            )
+    s({trig = ",C", dscr = "Codes block", snippetType = "autosnippet" },
+        fmt(
+            [[
+            ```<>
+            <>
+            ```
+            ]],
+            { i(1), i(2) }, { delimiters = "<>" }
+        )
     ),
 
     -- Mark
-    s(
-        { trig = ",m", dscr = "mark", wordtrig = false, priority = 1000, snippetType = "autosnippet" },
-        t("- [ ] ")
-    ),
+    s({trig = ",m", dscr = "mark", wordtrig = false, priority = 1000, snippetType = "autosnippet" }, t("- [ ] ")),
 
     -- Insert a picture, choice nodes
-    s(
-        {
-
-            trig = ",p",
-            dscr = "Insert Picture, choice nodes",
-            wordtrig = false,
-            priority = 1000,
-            snippetType = "autosnippet"
-        },
+    s({trig = ",p", dscr = "Insert Picture, choice nodes", wordtrig = false, snippetType = "autosnippet"},
         c(1, {
             fmt(
                 [[![<>](<>)]],
@@ -98,15 +71,15 @@ local snippets = {
             ),
             fmt(
                 [[
-                        <center>
-                        <img style="border-radius: 0.2125em;" src="../img/{}" width="{}%" height="auto">
-                        <div style="
-                        display: outline;
-                        font-style: italic;
-                        color: #666;
-                        padding: 2px;">Figure {} </div>
-                        </center>
-                        ]],
+                <center>
+                <img style="border-radius: 0.2125em;" src="../img/{}" width="{}%" height="auto">
+                <div style="
+                display: outline;
+                font-style: italic;
+                color: #666;
+                padding: 2px;">Figure {} </div>
+                </center>
+                ]],
                 {
                     i(1, "image-name"),
                     i(2, "width"),
@@ -118,14 +91,9 @@ local snippets = {
     ),
 
     -- Insert a link
-    s(
-        { trig = ",a", dscr = "Link", wordtrig = false, priority = 1000, snippetType = "autosnippet" },
-            fmta(
-            [[[<>](<>)]],
-            { i(1, "display name"), i(2, "link") }
-        )
+    s({trig = ",a", dscr = "Link", wordtrig = false, priority = 1000, snippetType = "autosnippet" },
+       fmta([[[<>](<>)]], { i(1, "display name"), i(2, "link") })
     )
-
 }
 
 return snippets
