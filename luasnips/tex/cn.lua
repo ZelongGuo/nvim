466443c2b3cdf0a7dc6a56ebce878fdb3c5fddcf
zelong <zelong.guo@outlook.com>
2024-12-21 19:49:44 +0100

    update template and shebang

--- luasnips/tex/cn.lua
+++ luasnips/tex/cn.lua
@@ -2,10 +2,41 @@ local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 -- Chinese Support
 return {
+    -- A template
+    --
+    s({ trig = "cn_template" },
+        -- Fonts, check PostScrip Name of the font book on your mac
+        -- PostScrip Name: Baoli SC, Hei, Kai, Libian Sc, STFangsong, STKaiti, STSong
+        fmta([[
+             % !TEX program = XeLaTeX
+             % !TEX encoding = UTF-8
+             \documentclass{report}
+             \usepackage[UTF8]{ctex}
+
+             % ------------- Font Settings -------------
+             % default use ctex defaults, uncomment to use your owns
+             % \setCJKmainfont[BoldFont=STHeiti,ItalicFont=STKaiti]{STKaiti}
+             % \setCJKsansfont[BoldFont=STHeiti]{STLibianSC-Regular}
+             % \setCJKmonofont{STFangsong}
+
+             % ------------- Line Separations -------------
+             \linespread{1.0}           % Single line separation
+             \setlength{\parskip}{1pt}   % Paragraph separation
+             \setlength{\parindent}{2em} % indent for first line in a new Paragraph
+
+             \begin{document}
+             <>
+             \end{document}
+             ]], { i(1) }
+        )
+    ),
     -- A shebang-like comment for Chinese
     s(
-        { trig = 'cn' },
-        t("%! TeX program = xelatex")
+        { trig = 'cn_shebang' },
+        {
+            t("% ! TeX program = XeLaTEX"),
+            t({ "", "% !TEX encoding = UTF-8" })
+        }
     ),
 
     -- ctex package
@@ -13,4 +44,5 @@ return {
         { trig = 'ctex' },
         { t("\\usepackage[UTF8]{ctex}") }
     )
+
 }
