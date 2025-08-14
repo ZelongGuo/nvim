24b7158199820b59f43abcad119b74a60235c81c
zelong <zelong.guo@outlook.com>
2025-08-14 17:06:33 +0800

    remap "I" to move 5 lines up in TOC buffer and avoid its built-in keymap conflict

--- lua/plugins/lang/vimtex.lua
+++ lua/plugins/lang/vimtex.lua
@@ -22,5 +22,27 @@ return {
         vim.g.vimtex_matchparen_enabled = 0 -- disable this option to speed vim up
         vim.g.vimtex_syntax_enabled = 0
         vim.g.vimtex_syntax_conceal_disable = 1
+    end,
+
+    --  After plugin loaded, change the keymap in TOC Buffer
+    config = function()
+        vim.g.vimtex_toc_enabled = 1
+
+        vim.g.vimtex_toc_config = {
+            name = "Table of Contents",
+            indent_levels = 1, -- enable indent of section levels
+
+            -- layers = { "content", "label", "todo", "include" },
+            layers = { "content", "label", "todo" }, -- include is hidden to avoid "I" keymap conflict
+            split_width = 40,
+        }
+
+        -- Open TOC and remap the key, I --> move 5 lines up
+        local function open_toc_and_remap()
+            vim.cmd("VimtexTocOpen")
+            vim.keymap.set("n", "I", "5k", { buffer = true, silent = true })
+        end
+
+        vim.keymap.set("n", ",lt", open_toc_and_remap, { silent = true })
     end
 }
