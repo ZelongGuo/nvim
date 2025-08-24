57303e84f68736c39ecd1cb1dea315f0b909dcd6
zelong <zelong.guo@outlook.com>
2025-08-24 13:50:56 +0800

    cancle using flash for / search mode

--- lua/plugins/editor/flash.lua
+++ lua/plugins/editor/flash.lua
@@ -7,22 +7,22 @@ return {
         labels = "jklasdfghqwertyuiopzxcvbnm",
 
         modes = {
-            search = { enabled = true },
+            search = { enabled = false }, -- otherwise / search cannnot work well
             char = { enabled = true },
         },
     },
     -- stylua: ignore
     keys = {
-        { "/", mode = { "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { ";", mode = { "n" }, function()
             require("flash").jump({
             search = { mode = "search", max_length = 0 },
             label = { after = { 0, 0 } },
             pattern = "^", })
         end, desc = "Jump Line" },
-        --   { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-          -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 }
